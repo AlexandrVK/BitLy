32b373dda47c94becb5582f1a024d9b23950340b
AlexandrVK <alexandrvk@gmail.com>
2023-04-03 14:28:44 +0300

    коррекция имени переменной

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ def shorten_link(token, user_input):
     url = "https://api-ssl.bitly.com/v4/bitlinks"
     payload = {"long_url": user_input}
     header = {"Authorization": f"Bearer {token}"}
-    response = requests.post(url, headers = header, json = payload)
+    response = requests.post(url, headers=header, json=payload)
     response.raise_for_status()
-  
+
     return response.json()['link']
 
 
@@ -20,44 +20,42 @@ def count_clicks(token, user_input):
 
     url = f"https://api-ssl.bitly.com/v4/bitlinks/{user_input}/clicks/summary"
     header = {"Authorization": f"Bearer {token}"}
-    response = requests.get(url, headers = header)
+    response = requests.get(url, headers=header)
     response.raise_for_status()
-  
+
     return response.json()['total_clicks']
 
 
 def is_bitlink(token, user_input):
-    temp_parsed_user_input = urlparse(user_input)
-    parsed_user_input = temp_parsed_user_input.path if not temp_parsed_user_input.netloc else f"{temp_parsed_user_input.netloc}{temp_parsed_user_input.path}"
+    parsed_url = urlparse(user_input)
+    parsed_user_input = parsed_url.path if not parsed_url.netloc else f"{parsed_url.netloc}{parsed_url.path}"
     url = f"https://api-ssl.bitly.com/v4/bitlinks/{parsed_user_input}"
     header = {"Authorization": f"Bearer {token}"}
-    response = requests.get(url, headers = header)
-    
+    response = requests.get(url, headers=header)
+
     return response.ok
 
 
 if __name__ == '__main__':
-    
+
     load_dotenv()
     token = os.environ['BITLY_TOKEN']
-    
-    
+
     parser = argparse.ArgumentParser(
         description='Прорамма обработки ссылок для сайта Bit.ly'
     )
     parser.add_argument('user_input', help='Введите ссылку:')
     args = parser.parse_args()
-        
+
     try:
         if is_bitlink(token, args.user_input):
-            temp_parsed_user_input = urlparse(args.user_input)
-            bitlink = temp_parsed_user_input.path if not temp_parsed_user_input.netloc else f"{temp_parsed_user_input.netloc}{temp_parsed_user_input.path}"
-            click_sum = count_clicks(token,bitlink)
+            parsed_url = urlparse(args.user_input)
+            bitlink = parsed_url.path if not parsed_url.netloc else f"{parsed_url.netloc}{parsed_url.path}"
+            click_sum = count_clicks(token, bitlink)
             print(f"По вашей ссылке прошли {click_sum} раз(а)")
-        else:  
+        else:
             bitlink = shorten_link(token, args.user_input)
             print('Битлинк:', bitlink)
-        
+
     except requests.exceptions.HTTPError:
         print('Ошибка: Неверная ссылка')
-        
